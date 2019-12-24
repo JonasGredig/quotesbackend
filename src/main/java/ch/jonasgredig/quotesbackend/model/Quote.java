5fb29e2a79f80a0811c523f9f105171ad8b1f256
Jonas Gredig <jongredig@gmail.com>
2019-12-24 22:58:18 +0100

    add model for Quote and User

---
+++ src/main/java/ch/jonasgredig/quotesbackend/model/Quote.java
@@ -0,0 +1,51 @@
+package ch.jonasgredig.quotesbackend.model;
+
+public class Quote {
+
+    private Long userId;
+    private String author;
+    private long timestamp;
+    private String quoteText;
+
+    public Quote() {
+    }
+
+    public Quote(Long userId, String author, long timestamp, String quoteText) {
+        this.userId = userId;
+        this.author = author;
+        this.timestamp = timestamp;
+        this.quoteText = quoteText;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getQuoteText() {
+        return quoteText;
+    }
+
+    public void setQuoteText(String quoteText) {
+        this.quoteText = quoteText;
+    }
+}
